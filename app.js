4a0970e0b82c1235d25c5085c63f6ecf22332968
Samuel Panek <samuelrpanek@gmail.com>
2021-10-18 15:01:06 -0700

    string does not need curlies for returns

--- app.js
+++ app.js
@@ -152,7 +152,9 @@ console.log(joe);
 
 
 
-Student.prototype.greeting = () => `Hi, my name is ${this.name}`;
+Student.prototype.greeting = function() {
+  return `Hi, my name is ${this.name}`;
+};
 
 // TODO: Uncomment the following line of code to see the output in the browser console
 // Note that the arrow function will cause this method to break!
